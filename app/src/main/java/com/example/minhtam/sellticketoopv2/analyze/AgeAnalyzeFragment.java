2efe9a26e6bf38b2925d6c4b8c97bd3e2119c83b
pminhtam <pminhtamnb@gmail.com>
2017-11-13 22:57:37 +0700

    Vẽ biểu đồ thống kê sử dụng MPAndroidChart

---
+++ app/src/main/java/com/example/minhtam/sellticketoopv2/analyze/AgeAnalyzeFragment.java
@@ -0,0 +1,32 @@
+package com.example.minhtam.sellticketoopv2.analyze;
+
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.minhtam.sellticketoopv2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ *
+ */
+public class AgeAnalyzeFragment extends Fragment {
+
+
+    public AgeAnalyzeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_age_analyze, container, false);
+    }
+
+}
