3f7d864da327b76d34947e2c3207d795ae39f1d0
pminhtam <pminhtamnb@gmail.com>
2017-11-19 15:53:13 +0700

    add schedule

---
+++ app/src/main/java/com/example/minhtam/sellticketoopv2/seller/createschedule/ChooseRoomAdapter.java
@@ -0,0 +1,8 @@
+package com.example.minhtam.sellticketoopv2.seller.createschedule;
+
+/**
+ * Created by Minh Tam on 11/19/2017.
+ */
+
+public class ChooseRoomAdapter {
+}
---
+++ app/src/main/java/com/example/minhtam/sellticketoopv2/seller/createschedule/CreateScheduleFragment.java
@@ -0,0 +1,30 @@
+package com.example.minhtam.sellticketoopv2.seller.createschedule;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.minhtam.sellticketoopv2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CreateScheduleFragment extends Fragment {
+
+
+    public CreateScheduleFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_schedule, container, false);
+    }
+
+}
